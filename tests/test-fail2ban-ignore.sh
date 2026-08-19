fa64cb7f1117ae45539e02d7f961eae0d69fdf0c
AndrewZAP1977 <31244624+AndrewZAP1977@users.noreply.github.com>
2026-08-19 16:36:37 +0300

    cleanup: use generic router ID in ignore tests

--- tests/test-fail2ban-ignore.sh
+++ tests/test-fail2ban-ignore.sh
@@ -12,7 +12,7 @@ mkdir -p \
 cp "${ROOT}/config/config.env.example" "${T}/etc/router-ip-push-hardening/config.env"
 printf '%s\n' '127.0.0.1/32 # localhost' '198.51.100.10/32 # static trusted test VPS' >"${T}/etc/router-ip-push-hardening/trusted-static.list"
 printf '%s\n' '{"version":1,"routers":{}}' >"${T}/etc/router-ip-push-hardening/previous-ip-grace.json"
-printf '%s\n' '192.0.2.25' >"${T}/var/lib/router-ip-push/ips/AX3200.ipv4"
+printf '%s\n' '192.0.2.25' >"${T}/var/lib/router-ip-push/ips/ROUTER_A.ipv4"
 
 "${IGNORE}" --root "${T}" 192.0.2.25
 "${IGNORE}" --root "${T}" 198.51.100.10
